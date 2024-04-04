09de8160c13b4853c093f11b3e255df2e696ad31
DevWebAbhi <tiwariabhishek889912@gmail.com>
2024-04-04 23:21:59 +0530

    Infinity Listing

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../Styles/LandingPage.css';
 import { uniqueNamesGenerator, Config, adjectives, colors, animals } from 'unique-names-generator';
 import { useDispatch, useSelector } from 'react-redux';
 import { SET_DATA } from '../Redux/actionType';
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams,useLocation} from 'react-router-dom';
 
 
 interface RootState {
@@ -18,6 +18,7 @@ interface DataObject {
 }
 
 const LandingPage = () => {
+    let location = useLocation();
   const dispatch = useDispatch();
   
   const selector = useSelector((state: RootState) => state);
@@ -38,13 +39,9 @@ const LandingPage = () => {
 
   const handleClick = (e)=>{
     e.preventDefault();
+    const lastId=Number(location.search.split("=")[1]);
     const temp_data=selector.data;
-    for(let i=0;i<temp_data.length;i++){
-        if(temp_data[i].background){
-            temp_data[i].background=false;
-            break;  
-        }
-    }
+    temp_data[lastId-1].background=false;
     temp_data[Number(e.target.className)-1].background=true;
     dispatch({type:SET_DATA,payload:temp_data});
     console.log(e.target.className);
@@ -53,11 +50,15 @@ const LandingPage = () => {
     
   }
 
-  useEffect(() => {
+  useEffect(()=>{
     setSearchParams({ id: 1 });
-    const data: DataObject[] = [];
+  },[])
 
-    for (let i = 0; i < 1000; i++) {
+  useEffect(() => {
+    
+    const data: DataObject[] = [...selector.data];
+
+    for (let i = 0; i < 10; i++) {
       const shortName: string = uniqueNamesGenerator(customConfig);
       const description: string = uniqueNamesGenerator(bigCustomConfig);
       const obj: DataObject = {
@@ -73,59 +74,83 @@ const LandingPage = () => {
 
     dispatch({ type: SET_DATA, payload: data });
 
-  }, [dispatch]);
+  }, []);
+
+  const [loadedUpTo, setLoadedUpTo] = useState(10);
+
+  const handleScroll = (e) => {
+    const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+    if (bottom) {
+      // Calculate the next batch size dynamically
+      const nextBatchSize = loadedUpTo + 10;
+      const newData: DataObject[] = [];
+      for (let i = loadedUpTo; i < nextBatchSize; i++) {
+        const shortName: string = uniqueNamesGenerator(customConfig);
+        const description: string = uniqueNamesGenerator(bigCustomConfig);
+        const obj: DataObject = {
+          name: shortName,
+          description: description,
+          background:false
+        };
+        
+        newData.push(obj);
+      }
+      setLoadedUpTo(nextBatchSize);
+      dispatch({ type: SET_DATA, payload: [...selector.data, ...newData] });
+    }
+  };
 
   return (
-    
     <>
-    <h1 className='heading'>User Data</h1>
-    <div className='main-box'>
-      <div>
-        {
-          selector.data.map((e: DataObject, index: number) => (
-            <div key={index} className={`${index+1}`} style={{background:e.background?"rgb(210, 210, 210)":"transparent",borderRight:e.background?"2px solid":"transparent"}} onClick={handleClick}>
-              <h1 className={`${index+1}`}>{e.name}</h1>
-              <p className={`${index+1}`}>{e.description}</p>
-            </div>
-          ))
-        }
-      </div>
+      <h1 className='heading'>User Data</h1>
+      <div className='main-box' >
+        <div onScroll={handleScroll}>
+          {
+            selector.data.map((e: DataObject, index: number) => (
+              <div key={index} className={`${index+1}`} style={{background:e.background?"rgb(210, 210, 210)":"transparent",borderRight:e.background?"2px solid":"transparent"}} onClick={handleClick}>
+                <h1 className={`${index+1}`}>{e.name}</h1>
+                <p className={`${index+1}`}>{e.description}</p>
+              </div>
+            ))
+          }
+        </div>
      
-      <div>
-       {
-        selector.data.map((e:DataObject,index : number)=>(
-            e.background==true?
-            <>
-            <h1>
-        {
-            selector.data[index]?selector.data[index].name:""
-        }
-       </h1>
-       <p>
-        {
-          selector.data[index]?selector.data[index].description:""  
-        }
-       </p>
-       <div className="image-grid">
-            {
-                selector.data[index]?<>
-              <img src={`https://random.imagecdn.app/${100+index}/150`} className='random-image' alt='random-image'/>
-              <img src={`https://random.imagecdn.app/${200+index}/150`} className='random-image' alt='random-image'/>
-              <img src={`https://random.imagecdn.app/${300+index}/150`} className='random-image' alt='random-image'/>
-              <img src={`https://random.imagecdn.app/${400+index}/150`} className='random-image' alt='random-image'/>
-              <img src={`https://random.imagecdn.app/${500+index}/150`} className='random-image' alt='random-image'/>
-              <img src={`https://random.imagecdn.app/${600+index}/150`} className='random-image' alt='random-image'/>
-              <img src={`https://random.imagecdn.app/${700+index}/150`} className='random-image' alt='random-image'/>
-              <img src={`https://random.imagecdn.app/${800+index}/150`} className='random-image' alt='random-image'/>
-              <img src={`https://random.imagecdn.app/${900+index}/150`} className='random-image' alt='random-image'/>
-                </>:<></>
-            }
-            </div>
-            </>:<></>
-        ))
-       }
+        <div>
+          {
+            selector.data.map((e:DataObject,index : number)=>(
+                e.background==true?
+                <React.Fragment key={index}>
+                  <h1>
+                    {
+                      selector.data[index] ? selector.data[index].name : ""
+                    }
+                  </h1>
+                  <p>
+                    {
+                      selector.data[index] ? selector.data[index].description : ""
+                    }
+                  </p>
+                  <div className="image-grid">
+                    {
+                      selector.data[index] ? <>
+                        <img src={`https://random.imagecdn.app/${100 + index}/150`} className='random-image' alt='random-image' />
+                        <img src={`https://random.imagecdn.app/${200 + index}/150`} className='random-image' alt='random-image' />
+                        <img src={`https://random.imagecdn.app/${300 + index}/150`} className='random-image' alt='random-image' />
+                        <img src={`https://random.imagecdn.app/${400 + index}/150`} className='random-image' alt='random-image' />
+                        <img src={`https://random.imagecdn.app/${500 + index}/150`} className='random-image' alt='random-image' />
+                        <img src={`https://random.imagecdn.app/${600 + index}/150`} className='random-image' alt='random-image' />
+                        <img src={`https://random.imagecdn.app/${700 + index}/150`} className='random-image' alt='random-image' />
+                        <img src={`https://random.imagecdn.app/${800 + index}/150`} className='random-image' alt='random-image' />
+                        <img src={`https://random.imagecdn.app/${900 + index}/150`} className='random-image' alt='random-image' />
+                      </> : <></>
+                    }
+                  </div>
+                </React.Fragment>
+                : <React.Fragment key={index}></React.Fragment>
+            ))
+          }
+        </div>
       </div>
-    </div>
     </>
   );
 };
